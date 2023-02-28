592f5385ed972bf256cea33b0f2af05ffc54c156
Kamil Wojcicki <kamilwojcickinjr@gmail.com>
2023-02-28 09:06:25 +0100

    Initial Commit

---
+++ unitConversion/unitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitConversionApp.swift
+//  unitConversion
+//
+//  Created by Kamil Wójcicki on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct unitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
